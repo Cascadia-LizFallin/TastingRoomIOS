d4cf725efbf778e72725e770cefee73ba4c14660
Fallin, Liz <lizfallin@cc1-230-302.student.cascadia.edu>
2016-05-25 15:58:09 -0700

    Basic menuing in place for Appearance, Nose, Palate

--- TastingRoomIOS/PalateTableViewController.swift
+++ TastingRoomIOS/PalateTableViewController.swift
@@ -1,47 +1,45 @@
 //
-//  AppearanceTableViewController.swift
+//  PalateTableViewController.swift
 //  TastingRoomIOS
 //
-//  Created by Elizabeth Fallin on 5/23/16.
+//  Created by Fallin, Liz on 5/25/16.
 //  Copyright © 2016 Liz Fallin. All rights reserved.
 //
 
 import UIKit
 
-class AppearanceTableViewController: UITableViewController {
-    
-    var listData = ["Clarity/Brightness", "Intensity", "Color"]
+class PalateTableViewController: UITableViewController {
 
+    var listData = ["Sweetness", "Acidity", "Tannin", "Body", "Flavor Characteristics", "Length"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
-
-
+    
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return listData.count
     }
-
+    
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-     let cell = tableView.dequeueReusableCellWithIdentifier("AppearanceCell", forIndexPath: indexPath)
-     cell.textLabel?.text = listData[indexPath.row];
-     return cell
+        let cell = tableView.dequeueReusableCellWithIdentifier("PalateCell", forIndexPath: indexPath)
+        cell.textLabel?.text = listData[indexPath.row];
+        return cell
     }
-    
 
-    
 }
